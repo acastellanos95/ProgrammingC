477f504aa6e06289ce4e387689f2e375c22f5711
acastellanos95 <acastellanosaldama@gmail.com>
2020-01-30 11:39:57 -0600

    fix output bugs in complement.c

--- practicas2/complement.c
+++ practicas2/complement.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
+#define MAXBITLENGTH 16
 
 int main(){
   int num;
-  char numbin[19]="0000000000000000000";
+  char numbin[MAXBITLENGTH];
+  numbin[MAXBITLENGTH+1]='\0';
   printf("Escribe un numero entero:\n");
   scanf(" %d", &num);
   if (num <= 65535 && num > 0){
     printf("Tu numero en decimal es: %d\n", num);
-    int i=0;
-    while(numbin[i] != '\0'){
-    	if(num & 32768 == 32768){
+    for(int i=MAXBITLENGTH; i>=0; i--){
+    	if(num & 1 == 1){
 	  numbin[i]='1';
 	}
 	else{
 	  numbin[i]='0';
 	}
-	num = num << 1;
-	i++;
+	num = num >> 1;
     }
     printf("Tu numero en binario es: %s\n", numbin);
   }
